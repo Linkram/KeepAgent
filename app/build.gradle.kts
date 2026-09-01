c298d7af0de4f8f8a46c2effe414ca6f4311ab50
KeepAgent <dev@keepagent.local>
2026-09-01 13:39:11 -0400

    version 0.1.0-m1.4h (code 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.keepagent"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.1.0-m1.4"
+        versionCode = 4
+        versionName = "0.1.0-m1.4h"
         vectorDrawables { useSupportLibrary = true }
     }
 
